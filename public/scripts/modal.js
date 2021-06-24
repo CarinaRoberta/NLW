1152128e268638b24fb9aca53f3023b665038843
CarinaRoberta <silva.carina@live.com>
2021-06-24 19:26:01 -0300

    implementando o database

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,6 +1,15 @@
-module.exports = function modal() {
-    function opem() { }
-    function close() { }
+export default function modal() {
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+    cancelButton.addEventListener("click", close)
+
+    function open() {
+        modalWrapper.classList.add('active')
+    }
+    function close() {
+        document.querySelector('.modal-wrapper').classList.remove('active')
+    }
 
     return { open, close }
 }
