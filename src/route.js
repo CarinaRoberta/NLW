347b0978ca9953349907409a12c9d5dd9b743bfb
CarinaRoberta <silva.carina@live.com>
2021-06-25 20:03:28 -0300

    finalizando db

--- src/route.js
+++ src/route.js
@@ -5,10 +5,16 @@ const roomController = require('./controllers/roomController')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", { page: 'enter-room' }))
-route.get('/room/:room', (req, res) => res.render("room"))
+
 route.get('/create-pass', (req, res) => res.render("index", { page: 'create-pass' }))
 
-route.post('/question/:room/:question/:action', questionController.index)
 route.post('/create-room', roomController.create)
+route.get('/room/:room', roomController.open)
+route.post('/enterroom', roomController.enter)
+
+
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action', questionController.index)
+
 
 module.exports = route
